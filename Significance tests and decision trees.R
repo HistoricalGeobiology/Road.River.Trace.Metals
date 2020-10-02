3184ba3c7ffb16c40fa0f027d2a49083e391d244
HistoricalGeobiology <71403927+HistoricalGeobiology@users.noreply.github.com>
2020-10-02 10:45:47 -0700

    Add files via upload

--- Significance tests and decision trees.R
+++ Significance tests and decision trees.R
@@ -114,92 +114,11 @@ printcp(DecTree)
 summary(DecTree)
 rpart.plot(DecTree, digits=-4, main="Euxinic-Ferruginous Mo/U classification tree")
 
-#Mo analyses
-#Filter for euxinic samples and samples with positive authigenic Mo
-Euxinic <- filter(geochem.anox, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-Pre467Euxinic <- filter(Pre467, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-MiddleEuxinic <- filter(Middle, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-Post408Euxinic <- filter(Post408, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(Pre467Euxinic$Mo.auth)
-shapiro.test(MiddleEuxinic$Mo.auth)
-shapiro.test(Post408Euxinic$Mo.auth)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(Pre467Euxinic$Mo.auth, MiddleEuxinic$Mo.auth)
-wilcox.test(MiddleEuxinic$Mo.auth,Post408Euxinic$Mo.auth)
-wilcox.test(Pre467Euxinic$Mo.auth, Post408Euxinic$Mo.auth)
-
-#Regression tree for authigenic Mo in global dataset
-RegTreeMoGlobal <- rpart(Euxinic$Mo.auth ~ interpreted_age, method="anova", data=Euxinic)
-printcp(RegTreeMoGlobal)
-summary(RegTreeMoGlobal)
-rpart.plot(RegTreeMoGlobal, digits=-4, main="Global authigenic Mo (euxinic samples) regression tree")
-
-#Mo/TOC analyses
-#No infinite Mo/TOC numbers, do not need to remove
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(Pre467Euxinic$Mo.TOC)
-shapiro.test(MiddleEuxinic$Mo.TOC)
-shapiro.test(Post408Euxinic$Mo.TOC)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(Pre467Euxinic$Mo.TOC, MiddleEuxinic$Mo.TOC)
-wilcox.test(MiddleEuxinic$Mo.TOC,Post408Euxinic$Mo.TOC)
-wilcox.test(Pre467Euxinic$Mo.TOC, Post408Euxinic$Mo.TOC)
-
-#Regression tree for Mo/TOC in global dataset
-RegTreeMoTOCGlobal <- rpart(Euxinic$Mo.TOC ~ interpreted_age, method="anova", data=Euxinic)
-printcp(RegTreeMoTOCGlobal)
-summary(RegTreeMoTOCGlobal)
-rpart.plot(RegTreeMoTOCGlobal, digits=-4, main="Global Mo/TOC (euxinic samples) regression tree")
-
-#U analyses
-#Filter for euxinic samples and samples with positive authigenic Mo
-AnoxicU <- filter(geochem.anox, U.auth >0)
-Pre467AnoxicU <- filter(Pre467, U.auth >0)
-MiddleAnoxicU <- filter(Middle, U.auth >0)
-Post408AnoxicU <- filter(Post408, U.auth >0)
-
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(Pre467AnoxicU$U.auth)
-shapiro.test(MiddleAnoxicU$U.auth)
-shapiro.test(Post408AnoxicU$U.auth)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(Pre467AnoxicU$U.auth, MiddleAnoxicU$U.auth)
-wilcox.test(MiddleAnoxicU$U.auth,Post408AnoxicU$U.auth)
-wilcox.test(Pre467AnoxicU$U.auth, Post408AnoxicU$U.auth)
-
-#Regression tree for authigenic U in global dataset
-RegTreeUGlobal <- rpart(AnoxicU$U.auth ~ interpreted_age, method="anova", data=AnoxicU)
-printcp(RegTreeUGlobal)
-summary(RegTreeUGlobal)
-rpart.plot(RegTreeUGlobal, digits=-4, main="Global authigenic U (anoxic samples) regression tree")
-
-#U/TOC analyses
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(Pre467AnoxicU$U.TOC)
-shapiro.test(MiddleAnoxicU$U.TOC)
-shapiro.test(Post408AnoxicU$U.TOC)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(Pre467AnoxicU$U.TOC, MiddleAnoxicU$U.TOC)
-wilcox.test(MiddleAnoxicU$U.TOC,Post408AnoxicU$U.TOC)
-wilcox.test(Pre467AnoxicU$U.TOC, Post408AnoxicU$U.TOC)
-
-#Regression tree for U/TOC in global dataset
-RegTreeUTOCGlobal <- rpart(AnoxicU$U.TOC ~ interpreted_age, method="anova", data=AnoxicU)
-printcp(RegTreeUTOCGlobal)
-summary(RegTreeUTOCGlobal)
-rpart.plot(RegTreeUTOCGlobal, digits=-4, main="Global U/TOC (anoxic samples) regression tree")
-
 ############
 ##Peel River analyses
 ############
 
-#Calculate proportions of euxinic samples on the Peel River and RI-07-07A (with oxides considered to represent weathered pyrite) and number of euxinic versus ferruginous samples in different time bins and conduct Chi square tests
+#Calculate proportions of euxinic samples on the Peel River and RI-07-07A and number of euxinic versus ferruginous samples in different time bins and conduct Chi square tests
 PeelPre467FeSpeciationChi <- c(173,34)
 PeelMiddleFeSpeciationChi <- c(317,47)
 dat <- rbind(PeelPre467FeSpeciationChi, PeelMiddleFeSpeciationChi)
@@ -221,7 +140,7 @@ printcp(DecTree)
 summary(DecTree)
 rpart.plot(DecTree, digits=-4, main="Peel River Euxinic-Ferruginous iron speciation classification tree")
 
-#Calculate proportions of euxinic samples on the Peel River and RI-07-07A and number of euxinic versus ferruginous samples in different time bins and conduct Chi square tests
+#Calculate proportions of euxinic samples on the Peel River and RI-07-07A (with oxides considered to represent weathered pyrite) and number of euxinic versus ferruginous samples in different time bins and conduct Chi square tests
 PeelPre467FeSpeciationOxidesChi <- c(80,127)
 PeelMiddleFeSpeciationOxidesChi <- c(215,149)
 dat <- rbind(PeelPre467FeSpeciationOxidesChi, PeelMiddleFeSpeciationOxidesChi)
@@ -237,7 +156,7 @@ PeelPost408FeSpeciationChi <- c(124,105)
 dat <- rbind(PeelPre467FeSpeciationChi, PeelPost408FeSpeciationChi)
 chisq.test(dat, correct=FALSE)
 
-#Decision tree for best split on proportion euxinic with iron speciation (all oxides considered to represent weathered pyrite) on the Peel River
+#Decision tree for best split on proportion euxinic samples from the Peel River with iron speciation (all oxides considered to represent weathered pyrite) on the Peel River
 DecTree <- rpart(Peel$euxinicOxideAndPyrite ~ interpreted_age, method="class", data=Peel)
 printcp(DecTree)
 summary(DecTree)
@@ -259,166 +178,10 @@ PeelPost408euxinicMoUchi <- c(59,65)
 dat <- rbind(PeelPre467euxinicMoUChi, PeelPost408euxinicMoUchi)
 chisq.test(dat, correct=FALSE)
 
+#Decision tree for best split on proportion euxinic with Mo/U ratios
 DecTree <- rpart(Peel$euxinicMoU ~ interpreted_age, method="class", data=Peel)
 printcp(DecTree)
 summary(DecTree)
 rpart.plot(DecTree, digits=-4, main="Peel River Euxinic-Ferruginous Mo/U classification tree")
 
-#Peel River trace metal analyses: Mo
-PeelEuxinic <- filter(Peel, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-PeelPre467Euxinic <- filter(PeelPre467, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-PeelMiddleEuxinic <- filter(PeelMiddle, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-PeelPost408Euxinic <- filter(PeelPost408, Fe.py.FeHR >= 0.7 & Mo.auth >0)
-
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(PeelPre467Euxinic$Mo.auth)
-shapiro.test(PeelMiddleEuxinic$Mo.auth)
-shapiro.test(PeelPost408Euxinic$Mo.auth)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(PeelPre467Euxinic$Mo.auth, PeelMiddleEuxinic$Mo.auth)
-wilcox.test(PeelMiddleEuxinic$Mo.auth,PeelPost408Euxinic$Mo.auth)
-wilcox.test(PeelPre467Euxinic$Mo.auth, PeelPost408Euxinic$Mo.auth)
-
-#Regression tree for authigenic Mo on the Peel River
-RegTreeMo <- rpart(PeelEuxinic$Mo.auth ~ interpreted_age, method="anova", data=PeelEuxinic)
-printcp(RegTreeMo)
-summary(RegTreeMo)
-rpart.plot(RegTreeMo, digits=-4, main="Peel River authigenic Mo (euxinic samples) regression tree")
-
-#Do not need to filter for infinite Mo/TOC values values as there are none on the Peel River
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(PeelPre467Euxinic$Mo.TOC)
-shapiro.test(PeelMiddleEuxinic$Mo.TOC)
-shapiro.test(PeelPost408Euxinic$Mo.TOC)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(PeelPre467Euxinic$Mo.TOC, PeelMiddleEuxinic$Mo.TOC)
-wilcox.test(PeelMiddleEuxinic$Mo.TOC,PeelPost408Euxinic$Mo.TOC)
-wilcox.test(PeelPre467Euxinic$Mo.TOC, PeelPost408Euxinic$Mo.TOC)
-
-#Regression tree for Mo/TOC ratios on the Peel River
-RegTreeMo.TOC <- rpart(PeelEuxinic$Mo.TOC ~ interpreted_age, method="anova", data=PeelEuxinic)
-printcp(RegTreeMo.TOC)
-summary(RegTreeMo.TOC)
-rpart.plot(RegTreeMo.TOC, digits=-4, main="Peel River Mo/TOC (euxinic samples) regression tree")
-
-#Peel River trace metal analyses: U
-#First remove samples with U.auth <0
-Peel <-filter(Peel, U.auth>0)
-PeelPre467 <-filter(PeelPre467, U.auth>0)
-PeelMiddle <-filter(PeelMiddle, U.auth>0)
-PeelPost408 <-filter(PeelPost408, U.auth>0)
-
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(PeelPre467$U.auth)
-shapiro.test(PeelMiddle$U.auth)
-shapiro.test(PeelPost408$U.auth)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(PeelPre467$U.auth, PeelMiddle$U.auth)
-wilcox.test(PeelMiddle$U.auth,PeelPost408$U.auth)
-wilcox.test(PeelPre467$U.auth, PeelPost408$U.auth)
-
-#Regression tree for authigenic U on the Peel River
-RegTreeU <- rpart(Peel$U.auth ~ interpreted_age, method="anova", data=Peel)
-printcp(RegTreeU)
-summary(RegTreeU)
-rpart.plot(RegTreeU, digits=-4, main="Peel River authigenic U (anoxic samples) regression tree")
-
-#Do not need to filter for infinite U/TOC values values as there are none on the Peel River
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(PeelPre467$U.TOC)
-shapiro.test(PeelMiddle$U.TOC)
-shapiro.test(PeelPost408$U.TOC)
-
-#Normality is rejected, verified by histograms, run Mann-Whitney/Wilcoxon test
-wilcox.test(PeelPre467$U.TOC, PeelMiddle$U.TOC)
-wilcox.test(PeelMiddle$U.TOC,PeelPost408$U.TOC)
-wilcox.test(PeelPre467$U.TOC, PeelPost408$U.TOC)
-
-#Regression tree for Mo/TOC ratios on the Peel River
-RegTreeU.TOC <- rpart(Peel$U.TOC ~ interpreted_age, method="anova", data=Peel)
-printcp(RegTreeU.TOC)
-summary(RegTreeU.TOC)
-rpart.plot(RegTreeU.TOC, digits=-4, main="Peel River U/TOC (anoxic samples) regression tree")
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Trace metal analyses
-#Filter to appropriate redox facies and time bins
-#Rename original data frame
-geochem_metal <-geochem
-
-#Add Mo/TOC and U/TOC to data frame and remove infinite values
-geochem_metal$Mo.TOC <- geochem_metal$Mo..ppm. / geochem_metal$TOC..wt..
-geochem_metal$U.TOC <- geochem_metal$U..ppm. / geochem_metal$TOC..wt..
-
-#Filter to appropriate redox bins (anoxic and euxinic, respectively)
-geochem_metal <- filter(geochem_metal, !(is.na(interpreted_age)))
-geochem.total.anox_metal <- filter(geochem_metal, FeHR.FeT > 0.38 | FeT.Al > 0.53)
-euxinic_metal <-filter(geochem_metal, FeHR.FeT >= 0.38 & Fe.py.FeHR >= 0.7)
-
-#Filter to pre- and post-408 Ma time bins
-PrePragianAnoxic_metal <-filter(geochem.total.anox_metal, interpreted_age>408)
-PostPragianAnoxic_metal <-filter(geochem.total.anox_metal, interpreted_age<=408)
-PrePragianEuxinic_metal <-filter(euxinic_metal, interpreted_age>408)
-PostPragianEuxinic_metal <-filter(euxinic_metal, interpreted_age<=408)
-
-#Filter infinite U/TOC and Mo/TOC values
-PrePragianAnoxic_metal_U.TOC <-filter(PrePragianAnoxic_metal, !(U.TOC == Inf))
-PostPragianAnoxic_metal_U.TOC <-filter(PostPragianAnoxic_metal, !(U.TOC == Inf))
-PrePragianEuxinic_metal_Mo.TOC <-filter(PrePragianEuxinic_metal, !(Mo.TOC == Inf))
-PostPragianEuxinic_metal_Mo.TOC <-filter(PostPragianEuxinic_metal, !(Mo.TOC == Inf))
-
-#Conduct Shapiro-Wilk tests for normality. Low values reject the null hypothesis that data come from a normal distribution
-shapiro.test(PrePragianAnoxic_metal$U..ppm.)
-shapiro.test(PostPragianAnoxic_metal$U..ppm.)
-shapiro.test(PrePragianEuxinic_metal$Mo..ppm.)
-shapiro.test(PostPragianEuxinic_metal$Mo..ppm.)
-shapiro.test(PrePragianAnoxic_metal_U.TOC$U.TOC)
-shapiro.test(PostPragianAnoxic_metal_U.TOC$U.TOC)
-shapiro.test(PrePragianEuxinic_metal_Mo.TOC$Mo.TOC)
-shapiro.test(PostPragianEuxinic_metal_Mo.TOC$Mo.TOC)
-
-#Normality is rejected, verified by histograms run Mann-Whitney/Wilcoxon test
-wilcox.test(PrePragianAnoxic_metal$U..ppm., PostPragianAnoxic_metal$U..ppm.)
-wilcox.test(PrePragianEuxinic_metal$Mo..ppm., PostPragianEuxinic_metal$Mo..ppm.)
-
-wilcox.test(PrePragianEuxinic_metal_Mo.TOC$Mo.TOC, PostPragianEuxinic_metal_Mo.TOC$Mo.TOC)
-wilcox.test(PrePragianAnoxic_metal_U.TOC$U.TOC, PostPragianAnoxic_metal_U.TOC$U.TOC)
-
-#Regression Trees for best splits in Mo, Mo/TOC, U, and U/TOC
-
-RegTreeMo <- rpart(euxinic_metal$Mo..ppm. ~ interpreted_age, method="anova", data=euxinic_metal)
-printcp(RegTreeMo)
-summary(RegTreeMo)
-rpart.plot(RegTreeMo, digits=-4, main="Mo regression tree")
-
-euxinic_metal <- filter(euxinic_metal, !(Mo.TOC == Inf))
-RegTreeMoTOC <- rpart(euxinic_metal$Mo.TOC ~ interpreted_age, method="anova", data=euxinic_metal)
-printcp(RegTreeMoTOC)
-summary(RegTreeMoTOC)
-rpart.plot(RegTreeMoTOC, digits=-4, main="Mo TOC regression tree")
-
-RegTreeU <- rpart(geochem.total.anox_metal$U..ppm. ~ interpreted_age, method="anova", data=geochem.total.anox_metal)
-printcp(RegTreeU)
-summary(RegTreeU)
-rpart.plot(RegTreeU, digits=-4, main="U regression tree")
-
-geochem.total.anox_metal.for.U.TOC <- filter(geochem.total.anox_metal, !(U.TOC == Inf))
-RegTreeUTOC <- rpart(U.TOC ~ interpreted_age, method="anova", data=geochem.total.anox_metal.for.U.TOC)
-printcp(RegTreeUTOC)
-summary(RegTreeUTOC)
-rpart.plot(RegTreeUTOC, digits=-4, main="U TOC regression tree")
 
